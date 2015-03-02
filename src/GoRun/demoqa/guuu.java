5e46e76392264e672705a801dad0c3010ba679df
Gurvinder Dhillon <gurvinder.dhillon@live.com>
2015-03-01 23:39:17 -0800

    First Push

---
+++ src/GoRun/demoqa/guuu.java
@@ -0,0 +1,7 @@
+package GoRun.demoqa;
+
+/**
+ * Created by Gurvinder on 3/1/2015.
+ */
+public class guuu {
+}
---
+++ src/GoRun/demoqa/testCases/TestCase_POF.java
@@ -0,0 +1,54 @@
+package GoRun.demoqa.testCases;
+
+/**
+ * Created by Gurvinder on 3/1/2015.
+ */
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import org.openqa.selenium.support.PageFactory;
+
+import GoRun.demoqa.pageObjects.Home_PG_POF;
+
+import GoRun.demoqa.pageObjects.LogIn_PG_POF;
+
+public class TestCase_POF {
+
+    static WebDriver driver;
+
+    public static void main(String[] args) throws InterruptedException{
+
+        driver = new FirefoxDriver();
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.get("http://www.store.demoqa.com");
+
+        // This is to Instantiate Home Page and LogIn Page class
+
+        Home_PG_POF HomePage = PageFactory.initElements(driver, Home_PG_POF.class);
+
+        LogIn_PG_POF LoginPage = PageFactory.initElements(driver, LogIn_PG_POF.class);
+
+        // Once both classes Initialised, use their Web Element Objects
+
+        HomePage.lnk_MyAccount.click();
+
+        LoginPage.txtbx_UserName.sendKeys("testuser_1");
+
+        LoginPage.txtbx_Password.sendKeys("Test@123");
+
+        LoginPage.btn_Login.click();
+
+        System.out.println(" Login Successfully, now it is the time to Log Off buddy.");
+
+        HomePage.lnk_LogOut.click();
+
+        driver.quit();
+
+    }
+
+}
